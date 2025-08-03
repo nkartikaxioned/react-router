e87a5753bb91e0af289d25861ee5d3c6f175f346
dshirlyaxioned <shirlyd@axioned.com>
2025-08-03 20:15:58 +0530

    feat: learning NavLink component and ways to style/add class for styling

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,39 @@
-
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
 
-  return "Header";
-}
+  const getActiveNavStyle = ({isActive}) => {
+    return {
+      color : isActive ? "blue" : "",
+    };
+  }
+
+  return (
+    <header>
+      <nav>
+        <ul className="header-nav">
+          <li>
+            <NavLink to="/" className={({ isActive }) => isActive ? "Link-Active" : ""}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/about"}
+              style={({ isActive }) => {
+                return {
+                  color: isActive ? "red" : "black",
+                };
+              }}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/contact"} style={getActiveNavStyle}>Contact Us</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
